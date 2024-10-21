3574742cd38b495806bb2ae47b1f9fd71c34b8cf
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2024-10-21 09:01:36 +0700

    feat: get all land & detail land by user

--- src/pages/LandPage/LandItem/LandItem.jsx
+++ src/pages/LandPage/LandItem/LandItem.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Card, Button} from 'antd';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
+import {getLandByID} from '../../../services/api';
 
 const {Meta} = Card;
 
-const LandItem = ({title, description, image}) => {
+const LandItem = ({id, title, description, image}) => {
 	let navigate = useNavigate();
+	const firstImage =
+		image[0] && image[0].type === 'image'
+			? image[0].string_url
+			: 'https://th.bing.com/th/id/OIP.zY924La7IC_Yuhwjl2_3wwHaHa?w=800&h=800&rs=1&pid=ImgDetMain';
+	console.log(firstImage);
 
 	return (
 		<Card
 			hoverable
-			cover={<img alt={title} src={image} style={{width: '100%', height: 260}} />}
+			cover={<img alt={title} src={firstImage} style={{width: '100%', height: 260}} />}
 			style={{width: '100%'}}
 		>
 			<Meta title={title} description={description} style={{textAlign: 'left'}} />
 			<Button
-				onClick={() => navigate('/land-detail/1')}
+				onClick={() => navigate(`/land-detail/${id}`)}
 				type="primary"
 				style={{marginTop: '30px', minWidth: '200px'}}
 				size="large"
